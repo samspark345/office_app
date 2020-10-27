289ce2a68ee18cc65890eb76c27939fd9f985447
Samuel Okakwu <sokak074@uottawa.ca>
2020-10-27 12:51:09 -0400

    made lots of changes to reduce  unclearity and redundant code

--- Office_app.py
+++ Office_app.py
@@ -1,162 +1,92 @@
-def welcome_message(name, greeting):
+def message(name, greeting):
     '''(str,str)->None
         input must be a string
         prints a message to the screen when called '''
-    add = "__"
-    starts = (len(name)+len(greeting)+len(add)*2+2)
+    indent = "__"
+    starts = (len(name)+len(greeting)+len(indent)*2+2)
     print()
     print(starts*"*")
     print("*"+(starts-2)*" "+"*")
-    print("*"+add+name+greeting+add+"*")
+    print("*", indent, name, greeting, indent, "*")
     print("*" + (starts - 2) * " " + "*")
     print(starts * "*")
     print()
 
 
 class employee:
-    def __init__(self, name, email, tag, work):
+    users = {"admin": [],
+             "maintenance": [],
+             "worker": []
+             }
 
+    def __init__(self, name, email, tag, work):
+        '''
+     gets the information provided by the employee
+    '''
         self.name = name
         self.email = email
         self.tag = tag
-        self.position = position
-
-
-class worker(employee):
-    '''
-     a class that inherits from the main employee class
-    '''
-    users = {}
+        self.work = work
 
     def add(self):
         '''
-        (None) -> None
-        adds a user to the worker systems
-        '''
-        worker.users[self.name.strip()] = (self.tag, self.email)
+     adds a user to the system of choice
+    '''
+        if self.work == "a":
+            self.users["admin"].append((self.name, self.tag, self.email))
+        elif self.work == "m":
+            self.users["maintenance"].append((self.name, self.tag, self.email))
+        elif self.work == "w":
+            self.users["worker"].append((self.name, self.tag, self.email))
 
     @classmethod
     def remove(cls, name):
         '''
         (None) -> None
-        removes a user from the maintainance systems
+        removes a user from the systems list
         '''
-        if name in worker.users:
-            del worker.users[name]
-            print("you are now logged out")
+
+        for value in cls.users.values():
+            for user in value:
+                if name in user:
+                    value.remove(user)
+                    print("you are now logged out")
 
     @classmethod
     def pr_user(cls):
         '''
         (None) -> None
-        displays the users using the worker systems
+        displays the users using each system
         '''
-        if len(worker.users) >= 1:
-            for keys in worker.users:
-                print(("admin systems users {}:{}").format(keys, worker.users[keys]))
+        if len(cls.users) >= 1:
+            for keys in cls.users:
+                print(("systems users {}:{}").format(keys, cls.users[keys]))
 
 
-class admin(employee):
-    '''
-     a class that inherits from the main employee class
-    '''
-
-    users = {}
-
-    def add(self):
-        '''
-        (None) -> None
-        adds a user to the admin systems
-        '''
-        admin.users[self.name.strip()] = (self.tag, self.email)
-
-    @classmethod
-    def remove(cls, name):
-        '''
-        (None) -> None
-        removes a user from the admin systems
-        '''
-        if name in admin.users:
-            del admin.users[name]
-            print("{} you are now logged out")
-
-    @classmethod
-    def pr_user(cls):
-        '''
-        (None) -> None
-        displays the users using the admin systems
-        '''
-        if len(admin.users) >= 1:
-            for keys in admin.users:
-                print(("admin systems users {}:{}").format(keys, admin.users[keys]))
-
-
-class maintainance(employee):
-    '''
-     a class that inherits from the main employee class
-    '''
-    users = {}
-
-    def add(self):
-        '''
-     adds a user to the maintainance systems
-    '''
-        maintainance.users[self.name.strip()] = (self.tag, self.email)
-
-    @classmethod
-    def remove(cls, name):
-        '''
-        (None) -> None
-        removes a user from the maintainance systems
-        '''
-
-        if name in maintainance.users:
-            del maintainance.users[name]
-            print("you are now logged out")
-    @classmethod
-    def pr_user(cls):
-        '''
-        (None) -> None
-        displays the users using the maintainance systems
-        '''
-        if len(maintainance.users) >= 1:
-            for keys in maintainance.users:
-                print(("maintainance systems users {}:{}").format(keys(), maintainance.users[keys]))
-
-
-welcome_message("Hello!!! welcome", "")
+message("Hello!!! welcome", "")
 i = 0
 shutdown = True
 while shutdown:
     question = input("what do you want to do toaday?\n (1) for login, (2) for logout, (3) to see current users and (4) to shutdown\n")
-    if question == "1":
-        name = input("Please what is your name? : ")
-        email = input("what is your email? : ")
-        tag = input("what is your tag? : ")
-        position = input("what system would you like to use?\n (a) for admin, (m) for maintainance, (w) for worker: ")
-
-        if position.lower().strip() == "a":
-            person = admin(name, email, tag, position)
-            person.add()
-        elif position.lower().strip() == "m":
-            person = maintainance(name, email, tag, position)
-            person.add()
-        elif position.lower().strip() == "w":
-            person = maintainance(name, email, tag, position)
-            person.add()
-    elif question == "2":
-        name = input("please provide your name")
-        admin.remove(name)
-        maintainance.remove(name)
-        worker.remove(name)
-    elif question == "3":
-        maintainance.pr_user()
-        admin.pr_user()
-        worker.pr_user()
-    elif question == "4":
-        welcome_message("Goodbye", "")
-        shutdown = False
+    if question.isalpha():
+        print("Option not available please try again")
     else:
-        print("Invalid option, please try again")
+        question = int(question)
+        if question == 1:
+            name = input("Please what is your full name? : ")
+            email = input("what is your email? : ")
+            tag = input("what is your tag? : ")
+            position = input("what system would you like to use?\n (a) for admin, (m) for maintainance, (w) for worker: ")
 
-    i = i + 1
+            person = employee(name, email, tag, position)
+            person.add()
+        elif question == 2:
+            name = input("please provide your full name")
+            employee.remove(name)
+        elif question == 3:
+            employee.pr_user()
+        elif question == 4:
+            message("Goodbye", "")
+            shutdown = False
+        else:
+            print("Invalid number, please try again")
